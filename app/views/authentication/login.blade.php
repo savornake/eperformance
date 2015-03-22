363b6baee0c595de2bdbbb138e67c2203cde101e
savornake <savornake@gmail.com>
2015-03-22 13:30:02 +0700

    rubah ke bhs indo

--- app/views/authentication/login.blade.php
+++ app/views/authentication/login.blade.php
@@ -10,17 +10,17 @@
 <div class="container">
 
   <form class="form-signin">
-    <h2 class="form-signin-heading">Please sign in</h2>
+    <h2 class="form-signin-heading">Silahkan Login</h2>
     <label for="inputEmail" class="sr-only">Email address</label>
-    <input type="text" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+    <input type="text" id="inputEmail" class="form-control" placeholder="masuk username" required autofocus>
     <label for="inputPassword" class="sr-only">Password</label>
-    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+    <input type="password" id="inputPassword" class="form-control" placeholder="password" required>
     <div class="checkbox">
       <label>
-        <input type="checkbox" value="remember-me"> Remember me
+        <input type="checkbox" value="remember-me"> Ingat Password
       </label>
     </div>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Masuk</button>
   </form>
 
 </div> <!-- /container -->

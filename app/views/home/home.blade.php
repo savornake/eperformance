4683daf80b8509b085ef28b101126f23323ec7b7
savornake <savornake@gmail.com>
2015-04-16 14:21:43 +0700

    menambahkan tombol jadi 6 di menu home

--- app/views/home/home.blade.php
+++ app/views/home/home.blade.php
@@ -13,31 +13,47 @@
 
       <!-- Three columns of text below the carousel -->
       <div class="row text-center">
-        <div class="col-xs-6">
+        <div class="col-md-4">
           <img class="img-circle" src="img/ombudsman1.jpeg" alt="Generic placeholder image" width="140" height="140">
-          <h2>Renstra</h2>
-          <p>Menu ini berfungsi untuk menampilkan detail Renstra atau Rencana Strategis</p>
+          <h3>Rencana Strategis</h3>
+          <p>detail Renstra atau Rencana Strategis</p>
           <p><a class="btn btn-default" {{HTML::linkRoute('renstras.index','Detail Renstra')}} </a></p>
         </div><!-- /.col-lg-4 -->
-        <div class="col-xs-6">
+
+        <div class="col-md-4">
           <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h2>Menu 2</h2>
+          <h3>Penetapan Kinerja</h3>
           <p> Penjelasan Menu nomer 2 </p>
           <p><a class="btn btn-default" {{HTML::linkRoute('penetapan-kinerja.index','Detail Tapkin')}} </a></p>
         </div><!-- /.col-lg-4 -->
-        <div class="col-xs-6">
+
+        <div class="col-md-4">
           <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h2>Menu 3</h2>
+          <h3>Rencana Kegiatan Tahunan</h3>
           <p>Penjelasan Menu</p>
           <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
 
-         <div class="col-xs-6">
+         <div class="col-md-4">
           <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-          <h2>Menu 4</h2>
+          <h3>Capaian Penetapan Kinerja</h3>
           <p>Penjelasan Menu</p>
           <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
-      </div><!-- /.row -->
+
+		<div class="col-md-4">
+          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+          <h3>Laporan Realisasi Anggaran</h3>
+          <p>Penjelasan Menu</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+
+      	<div class="col-md-4">
+          <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+          <h3>Rencana Penyerapan Anggaran</h3>
+          <p>Penjelasan Menu</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+      </div>
 
 @stop

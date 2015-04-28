708aaf019198bebe6c0ae354f76dfd941b032a89
fathur <fathur_rohman17@yahoo.co.id>
2015-04-28 20:31:23 +0700

    change order

--- app/views/layout/menu.blade.php
+++ app/views/layout/menu.blade.php
@@ -21,8 +21,8 @@
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Menu <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
             <li>{{ HTML::linkRoute('renstra.index', 'Rencana Strategis') }}</li>
-            <li>{{ HTML::linkRoute('rkt.index', 'Rencana Kegiatan Tahunan') }}</li>
             <li>{{ HTML::linkRoute('tapkin.index', 'Penetapan Kinerja') }}</li>
+            <li>{{ HTML::linkRoute('rkt.index', 'Rencana Kegiatan Tahunan') }}</li>
             <li><a href="#">Capaian Penetapan Kinerja</a></li>
             <li><a href="#">Anggaran</a></li>
           </ul>
